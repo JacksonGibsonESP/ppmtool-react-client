a1de665c7fad19ab1889b3c015801e2d01847d0d
Nikita <nikich911@gmail.com>
2019-03-18 10:59:00 +0300

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashBoard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
     );
   }
 }
