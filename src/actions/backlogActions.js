001a8245f116e3afdc15b1c0c38273da1abb25d4
Nikita <JacksonGibsonESP@gmail.com>
2019-08-02 10:37:28 +0300

    Project Task

---
+++ src/actions/backlogActions.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { GET_ERRORS } from "./types";
+
+export const addProjectTask = (
+  backlogId,
+  projectTask,
+  history
+) => async dispatch => {
+  try {
+    await axios.post(`/api/backlog/${backlogId}`, projectTask);
+    history.push(`/projectBoard/${backlogId}`);
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
+import backlogReduer from "./backlogReduer";
 
 export default combineReducers({
   errors: errorReducer,
-  project: projectReducer
+  project: projectReducer,
+  backlog: backlogReduer
 });
