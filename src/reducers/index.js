75e3584d496cf0f32857b95cb605e5ebf192eccf
JacksonGibsonESP <JacksonGibsonESP@gmail.com>
2019-08-28 10:49:32 +0300

    Login happy path

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
 import backlogReduer from "./backlogReduer";
+import securityReducer from './securityReducer'
 
 export default combineReducers({
   errors: errorReducer,
   project: projectReducer,
-  backlog: backlogReduer
+  backlog: backlogReduer,
+  security: securityReducer
 });
